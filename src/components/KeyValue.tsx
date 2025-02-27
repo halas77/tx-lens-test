389d58d60d6194ed6efb8d180eea1259b68c4ad3
Dawit Mellese <dawitm777@gmail.com>
2025-02-27 14:09:10 +0300

    feat: enhance KeyValue component with copy-to-clipboard functionality and feedback indicator

--- src/components/KeyValue.tsx
+++ src/components/KeyValue.tsx
@@ -1,10 +1,9 @@
-import { ClipboardIcon } from "@heroicons/react/24/outline";
-import React from "react";
+import { ClipboardIcon, CheckIcon } from "@heroicons/react/24/outline";
+import React, { useState } from "react";
 
 /**
  * KeyValue component displays a label and value with a copy-to-clipboard button.
  */
-
 
 const KeyValue = ({
   label,
@@ -12,23 +11,36 @@ const KeyValue = ({
 }: {
   label: string;
   value: string | React.ReactNode;
-}) => (
-  <div className="flex justify-between items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg">
-    <span className="text-sm text-gray-500 dark:text-gray-300">{label}</span>
-    <div className="flex items-center space-x-2">
-      {typeof value === "string" ? (
-        <span className="text-sm font-medium">{value}</span>
-      ) : (
-        value
-      )}
-      <button
-        className="text-gray-400 hover:text-gray-600 dark:hover:text-gray-200"
-        onClick={() => navigator.clipboard.writeText(value?.toString() || "")}
-      >
-        <ClipboardIcon className="h-4 w-4" />
-      </button>
+}) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(value?.toString() || "");
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 2000);
+  };
+  return (
+    <div className="flex justify-between items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg">
+      <span className="text-sm text-gray-500 dark:text-gray-300">{label}</span>
+      <div className="flex items-center space-x-2">
+        {typeof value === "string" ? (
+          <span className="text-sm font-medium">{value}</span>
+        ) : (
+          value
+        )}
+        <button
+          className="text-gray-400 hover:text-gray-600 dark:hover:text-gray-200"
+          onClick={handleCopy}
+        >
+          {isCopied ? (
+            <CheckIcon className="h-4 w-4 text-green-400" />
+          ) : (
+            <ClipboardIcon className="h-4 w-4" />
+          )}
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default KeyValue;
